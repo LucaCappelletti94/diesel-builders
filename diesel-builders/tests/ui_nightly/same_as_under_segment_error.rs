a14e6492bf157f6cb884da48eaaf91835f99f1be
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2026-09-08 12:40:47 +0200

    Add a trybuild test for the same_as under-segment error

---
+++ diesel-builders/tests/ui_nightly/same_as_under_segment_error.rs
@@ -0,0 +1,21 @@
+use diesel_builders::prelude::*;
+
+#[derive(Debug, Clone, PartialEq, Queryable, Selectable, Identifiable, TableModel)]
+#[diesel(table_name = mandatory_table)]
+pub struct Mandatory {
+    id: i32,
+    parent_id: i32,
+    mandatory_field: String,
+}
+
+#[derive(Debug, Clone, PartialEq, Queryable, Selectable, Identifiable, TableModel)]
+#[diesel(table_name = child_table)]
+pub struct Child {
+    // The `#[same_as(...)]` path is a bare column with no `table::` prefix.
+    #[same_as(parent_id)]
+    id: i32,
+    #[mandatory(mandatory_table)]
+    mandatory_id: i32,
+}
+
+fn main() {}
